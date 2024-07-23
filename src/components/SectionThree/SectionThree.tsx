fddfabfb5b9c0474fa48eb5fe35acfd08fdf9b47
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-07-23 21:26:48 +0530

    fixed section three

--- src/components/SectionThree/SectionThree.tsx
+++ src/components/SectionThree/SectionThree.tsx
@@ -8,7 +8,9 @@ const SectionThree = () => {
         Kind words from Kind souls
       </div>
       {/* whole wrapper */}
-      <div className="flex  gap-8 overflow-x-hidden ">
+      <div className="flex gap-8 overflow-hidden relative">
+        <div className="absolute right-0 top-0 w-2/12 h-[44rem] z-[2] gradient-right"></div>
+        <div className="absolute left-0 top-0 w-2/12 h-[44rem] z-[2] gradient-left"></div>
         <TestimonialContainer />
         <TestimonialContainer />
         <TestimonialContainer />
--- src/components/SectionThree/TestimonialContainer.tsx
+++ src/components/SectionThree/TestimonialContainer.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 const TestimonialContainer = () => {
   return (
-    <div className="border-2 border-red relative p-2 rounded-2xl">
-      <div className="absolute top-0 left-4 text-white text-9xl">"</div>
-      <div className="bg-orange min-w-[22rem] py-20 text-white p-4 text-balance rounded-xl">
-        Ramit is an exceptional full-stack developer whom I've had the pleasure
-        of collaborating with on numerous projects and hackathons. His technical
-        prowess, creativity, and dedication consistently shine through in every
-        task he undertakes. Ramit's ability to navigate complex challenges with
-        ease and deliver innovative solutions makes him a valuable asset to any
-        team.
-      </div>
-      <div className="flex p-4 gap-4">
-        <div className="w-16 h-16 rounded-full text-white bg-green"></div>
-        <div className="flex flex-col">
-          <div className="text-xl text-white">Ramit Vishwakarma</div>
-          <div className="text-sm text-white">SDE1 @ Google</div>
+    <div className="infinite-carousel">
+      <div className="border-2 border-red relative p-2 rounded-2xl">
+        <div className="absolute top-0 left-4 text-white text-9xl">"</div>
+        <div className="bg-orange min-w-[22rem] py-20 text-white p-4 text-balance rounded-xl">
+          Ramit is an exceptional full-stack developer whom I've had the
+          pleasure of collaborating with on numerous projects and hackathons.
+          His technical prowess, creativity, and dedication consistently shine
+          through in every task he undertakes. Ramit's ability to navigate
+          complex challenges with ease and deliver innovative solutions makes
+          him a valuable asset to any team.
+        </div>
+        <div className="flex p-4 gap-4">
+          <div className="w-16 h-16 rounded-full text-white bg-green"></div>
+          <div className="flex flex-col">
+            <div className="text-xl text-white">Ramit Vishwakarma</div>
+            <div className="text-sm text-white">SDE1 @ Google</div>
+          </div>
         </div>
       </div>
     </div>
