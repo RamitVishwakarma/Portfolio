6c7bfdc2082b1c4e9cc0a5c34cb29b0093dad47e
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-07-24 16:39:36 +0530

    Fixed imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Introduction from "../components/SectionOne/Introduction";
+
 import Testimonial from "@/components/Testimonial/Testimonial";
 import Wrapper from "@/components/Wrapper";
 import Contact from "@/components/Contact/Contact";
 import MoreAboutMe from "@/components/MoreAboutMe/MoreAboutMe";
-import Projects from "@/components/SectionTwo/Projects";
 import TechStack from "@/components/TechStack/TechStack";
+import Introduction from "@/components/Introduction/Introduction";
+import Projects from "@/components/Projects/Projects";
 
 const page = () => {
   return (
