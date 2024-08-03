246b4bf90d7999cd45a5ec32e6b466406fe339f4
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-08-03 16:42:40 +0530

    Made tech stack section responsive

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -9,7 +9,7 @@ const TechStack = () => {
         <h2 className="text-6xl text-white pb-10 font-Anton text-center">
           Tech Stack I Use Daily
         </h2>
-        <div className="grid grid-cols-3 gap-8 justify-center p-2 ">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8 justify-center p-2 ">
           <Container data={data} />
         </div>
       </div>
