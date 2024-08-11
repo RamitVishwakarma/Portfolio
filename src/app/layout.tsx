ffc95b102c6d5cdbcf8128c93fd193a345f08cea
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-08-11 14:04:31 +0530

    small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { Head } from "next/document";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Portfolio website | Ramit Vishwakarma",
