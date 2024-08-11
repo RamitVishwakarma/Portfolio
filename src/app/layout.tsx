66aebf3fa23b8abb9d7796eb1c857985a2549290
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-08-11 14:07:26 +0530

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Portfolio website | Ramit Vishwakarma",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
